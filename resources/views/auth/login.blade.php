8124316fd431e5a2c8000d6ac008a70353cbe9d3
wom987 <45610462+wom987@users.noreply.github.com>
2022-03-26 16:04:46 -0600

    card header removed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,8 +5,6 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">{{ __('Login') }}</div>
-
                     <div class="card-body">
                         <div class="row">
                             <div class="col-6">
