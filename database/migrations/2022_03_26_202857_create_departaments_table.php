7324586a24a922f108d24ce396bc637533b56ef1
marioguidos <82772404+marioguidos@users.noreply.github.com>
2022-03-26 14:34:15 -0600

    controller y tabla departamento

--- database/migrations/2022_03_26_202857_create_departaments_table.php
+++ database/migrations/2022_03_26_202857_create_departaments_table.php
@@ -13,16 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('departaments', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('departament')->nullable();
-            $table->string('web_site')->nullable();
-            $table->double('salary')->nullable();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('postal_code');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
@@ -34,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('departaments');
     }
 };
