6a4ab57252a223f8ba48ac8581e1b7d32693b56f
wom987 <45610462+wom987@users.noreply.github.com>
2022-03-26 14:24:01 -0600

    seeders done

--- app/Models/InvestigationTag.php
+++ app/Models/InvestigationTag.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Tags extends Model
+class InvestigationTag extends Model
 {
     use HasFactory;
 }
--- database/seeders/InvestigationTagsSeeder.php
+++ database/seeders/InvestigationTagsSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\InvestigationTag;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,21 @@ class InvestigationTagsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $investigation_tags = [
+            [
+                'investigation_id' => 1,
+                'tag_id' => 1
+            ], [
+                'investigation_id' => 2,
+                'tag_id' => 2
+            ], [
+                'investigation_id' => 3,
+                'tag_id' => 3
+            ], [
+                'investigation_id' => 4,
+                'tag_id' => 4
+            ]
+        ];
+        InvestigationTag::insert($investigation_tags);
     }
 }
